e5b9541c540ff40381847ca44e142b705e1018cc
caphodel <caphodel@gmail.com>
2015-03-24 11:50:05 +0700

    add camel case support

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ A Brackets extension that add additional functionality to Brackets editor, like 
 
 Works on windows 7!!! :)
 
-Now support select all, UPPERCASE and lowercase and some functionality from edit menu. Now ome menu items will be disabled if no text selected.
+Now support select all, Camel Case, UPPERCASE and lowercase and some functionality from edit menu. Now ome menu items will be disabled if no text selected.
--- main.js
+++ main.js
@@ -26,6 +26,8 @@ define(function (require, exports, module) {
         RIGHT_CLICK_MENU_UPPERCASE_COMMAND_ID  = "rightclickmenu.uppercase",
         RIGHT_CLICK_MENU_LOWERCASE_NAME   = "lowercase",
         RIGHT_CLICK_MENU_LOWERCASE_COMMAND_ID  = "rightclickmenu.lowercase",
+        RIGHT_CLICK_MENU_CAMELCASE_NAME   = "Camel Case",
+        RIGHT_CLICK_MENU_CAMELCASE_COMMAND_ID  = "rightclickmenu.camelcase",
         RIGHT_CLICK_MENU_SELECTALL_NAME   = "Select all",
         RIGHT_CLICK_MENU_SELECTALL_COMMAND_ID  = "rightclickmenu.selectall",
         RIGHT_CLICK_MENU_BLOCKCOMMENT_NAME   = "Toggle Block Comment",
@@ -34,7 +36,7 @@ define(function (require, exports, module) {
         RIGHT_CLICK_MENU_LINECOMMENT_COMMAND_ID  = "rightclickmenu.lineComment";
 
     $('#editor-holder').mousedown(function(event) {
-        var el = $(event.target)
+        var el = $(event.target);
 
         switch (event.which) {
             case 1:
@@ -73,6 +75,12 @@ define(function (require, exports, module) {
                         if($('#editor-context-menu-rightclickmenu\\.lowercase\\.disabled').length===0){
                             $('<li>'+li.html().replace('editor-context-menu-rightclickmenu.lowercase', 'editor-context-menu-rightclickmenu.lowercase.disabled')+'</li>').insertBefore(li).find('a').css('color', '#494949');
                         }
+
+                        li = $('#editor-context-menu-rightclickmenu\\.camelcase').parent();
+                        li.hide();
+                        if($('#editor-context-menu-rightclickmenu\\.camelcase\\.disabled').length===0){
+                            $('<li>'+li.html().replace('editor-context-menu-rightclickmenu.camelcase', 'editor-context-menu-rightclickmenu.camelcase.disabled')+'</li>').insertBefore(li).find('a').css('color', '#494949');
+                        }
                     }
                     else{
                         li = $('#editor-context-menu-rightclickmenu\\.copy').parent();
@@ -90,6 +98,10 @@ define(function (require, exports, module) {
                         li = $('#editor-context-menu-rightclickmenu\\.uppercase').parent();
                         li.show();
                         $('#editor-context-menu-rightclickmenu\\.uppercase\\.disabled').remove();
+
+                        li = $('#editor-context-menu-rightclickmenu\\.camelcase').parent();
+                        li.show();
+                        $('#editor-context-menu-rightclickmenu\\.camelcase\\.disabled').remove();
                     }
                 }
                 break;
@@ -142,6 +154,17 @@ define(function (require, exports, module) {
         currentDoc.replaceRange(selectedText.toLowerCase(), pos.start, pos.end);
     }
 
+    function camelcase(){
+        var editor = EditorManager.getCurrentFullEditor(),
+            selectedText = editor.getSelectedText().replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function(match, index) {
+                if (+match === 0) return ""; // or if (/\s+/.test(match)) for white spaces
+                return index === 0 ? match.toLowerCase() : match.toUpperCase();
+            }),
+            pos = editor.getSelection(),
+            currentDoc = DocumentManager.getCurrentDocument();
+        currentDoc.replaceRange(selectedText, pos.start, pos.end);
+    }
+
     function blockComment(){
         CommandManager.execute(Commands.EDIT_BLOCK_COMMENT);
     }
@@ -158,6 +181,7 @@ define(function (require, exports, module) {
     CommandManager.register(RIGHT_CLICK_MENU_PASTE_NAME, RIGHT_CLICK_MENU_PASTE_COMMAND_ID, pasteToEditor);
     CommandManager.register(RIGHT_CLICK_MENU_UPPERCASE_NAME, RIGHT_CLICK_MENU_UPPERCASE_COMMAND_ID, uppercase);
     CommandManager.register(RIGHT_CLICK_MENU_LOWERCASE_NAME, RIGHT_CLICK_MENU_LOWERCASE_COMMAND_ID, lowercase);
+    CommandManager.register(RIGHT_CLICK_MENU_CAMELCASE_NAME, RIGHT_CLICK_MENU_CAMELCASE_COMMAND_ID, camelcase);
     CommandManager.register(RIGHT_CLICK_MENU_SELECTALL_NAME, RIGHT_CLICK_MENU_SELECTALL_COMMAND_ID, selectall);
     CommandManager.register(RIGHT_CLICK_MENU_BLOCKCOMMENT_NAME, RIGHT_CLICK_MENU_BLOCKCOMMENT_COMMAND_ID, blockComment);
     CommandManager.register(RIGHT_CLICK_MENU_LINECOMMENT_NAME, RIGHT_CLICK_MENU_LINECOMMENT_COMMAND_ID, lineComment);
@@ -172,6 +196,7 @@ define(function (require, exports, module) {
     Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU).addMenuItem(RIGHT_CLICK_MENU_PASTE_COMMAND_ID);
     Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU).addMenuItem(RIGHT_CLICK_MENU_SELECTALL_COMMAND_ID);
     Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU).addMenuDivider();
+    Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU).addMenuItem(RIGHT_CLICK_MENU_CAMELCASE_COMMAND_ID);
     Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU).addMenuItem(RIGHT_CLICK_MENU_UPPERCASE_COMMAND_ID);
     Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU).addMenuItem(RIGHT_CLICK_MENU_LOWERCASE_COMMAND_ID);
     Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU).addMenuDivider();
